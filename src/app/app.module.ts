630dd90faadce1ca20f0eab521d97c76a6a0056d
darbro96 <dbronikowsk@gmail.com>
2021-02-08 00:11:04 +0100

    Zabranie userowi dostępu do profili użytkowników oraz opcji dodawania projektu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card'; 
+import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input'; 
-import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { ProjektyComponent } from './projekty/projekty.component';
+import {ProjektyComponent} from './projekty/projekty.component';
 import {MatTableModule} from '@angular/material/table';
-import { ZadanieComponent } from './zadanie/zadanie.component'; 
-import { HttpInterceptorService } from './http-interceptor.service';
-import { ProjektComponent } from './projekt/projekt.component';
-import { StudentComponent } from './student/student.component';
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
+import {ZadanieComponent} from './zadanie/zadanie.component';
+import {HttpInterceptorService} from './http-interceptor.service';
+import {ProjektComponent} from './projekt/projekt.component';
+import {StudentComponent} from './student/student.component';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { DodajProjektComponent } from './dodaj-projekt/dodaj-projekt.component';
+import {DodajProjektComponent} from './dodaj-projekt/dodaj-projekt.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,11 +50,11 @@ import { DodajProjektComponent } from './dodaj-projekt/dodaj-projekt.component';
     MatTableModule,
     TextFieldModule,
     MatDatepickerModule,
-    MatNativeDateModule, 
-    MatSelectModule
+    MatNativeDateModule,
+    MatSelectModule,
   ],
   providers: [
-    MatDatepickerModule, 
+    MatDatepickerModule,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpInterceptorService,
@@ -62,4 +63,5 @@ import { DodajProjektComponent } from './dodaj-projekt/dodaj-projekt.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
