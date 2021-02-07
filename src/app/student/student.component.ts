cd87889fc9ef696bbe07dc9380c322545fc28ea7
darbro96 <dbronikowsk@gmail.com>
2021-02-08 00:20:22 +0100

    Poprawka - brak dostępu usera do profili użytkowników

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { StudentService } from './student.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthenticationService } from '../authentication.service';
+import {Component, OnInit} from '@angular/core';
+import {StudentService} from './student.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthenticationService} from '../authentication.service';
 
 @Component({
   selector: 'app-student',
@@ -10,25 +10,33 @@ import { AuthenticationService } from '../authentication.service';
 })
 export class StudentComponent implements OnInit {
   constructor(public studentService: StudentService, private route: ActivatedRoute, private router: Router,
-              private authenticationService: AuthenticationService ) { }
+              private authenticationService: AuthenticationService) {
+  }
+
   student: any;
   isLoggedIn = false;
+  role!: any;
 
   ngOnInit(): void {
     this.isLoggedIn = this.authenticationService.isUserLoggedIn();
     if (!this.isLoggedIn) {
       this.router.navigate(['/login']);
+    } else {
+      this.role = this.authenticationService.getRole();
+      if (this.role === 'ROLE_USER') {
+        this.router.navigate(['/projekty']);
+      }
     }
     this.route.params.subscribe(data => {
       this.getStudent(data.student_id);
-    })
+    });
   }
 
   getStudent(student_id: number) {
     this.student = this.studentService.getStudent(student_id).subscribe(data => {
       this.student = data;
       console.log(this.student);
-    })
+    });
   }
 
 
